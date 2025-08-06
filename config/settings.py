dbd7de9a0e9f89e8056803d4d83a20da0c6fc8c2
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-08-06 13:49:50 +0300

    feat: Added Custom User Model and Configured it in settings.py to tell django to use it

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+# tell Django to use this custom user model:
+AUTH_USER_MODEL = 'users.User'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
